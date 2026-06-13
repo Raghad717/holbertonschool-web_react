093f6399d09abf3006fa8ddd8a6592755ad12d8e
Raghad717 <r4gh4dalmalki@gmail.com>
2026-06-13 12:38:27 +0300

    Fix formatting and remove unnecessary comments in interface.ts

--- TypeScript/task_3/js/interface.ts
+++ TypeScript/task_3/js/interface.ts
@@ -1,9 +1,7 @@
-// Create a type RowID and set it equal to number
 export type RowID = number;
 
-// Create an interface RowElement
 export interface RowElement {
   firstName: string;
   lastName: string;
-  age?: number; // Optional field
+  age?: number;
 }
