1df3f775f585c9890fe046671f1303fe8435cffd
Raghad717 <r4gh4dalmalki@gmail.com>
2026-06-13 12:37:09 +0300

    Create crud.d.ts for CRUD function declarations
    Added TypeScript declaration file for CRUD operations.

---
+++ TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,16 @@
+// Import RowID and RowElement from interface.ts
+import { RowID, RowElement } from './interface';
+
+// Declare the CRUD functions
+declare module './crud' {
+  export function insertRow(row: RowElement): RowID;
+  export function deleteRow(rowId: RowID): void;
+  export function updateRow(rowId: RowID, row: RowElement): RowID;
+}
+
+// Alternative: Using ambient namespace declaration
+declare namespace CRUD {
+  function insertRow(row: RowElement): RowID;
+  function deleteRow(rowId: RowID): void;
+  function updateRow(rowId: RowID, row: RowElement): RowID;
+}
